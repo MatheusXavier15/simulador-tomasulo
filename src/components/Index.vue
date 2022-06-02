aa110bab5ceb29ab9d8eecb35adf5257c748308e
Matheus Xavier <matheus.oliveira@rhitmo.com.br>
2022-06-01 23:02:18 -0300

    First Commit

---
+++ src/components/Index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="hello">
+    <div class="options-content">
+      <register-insert v-for="n in 10" :key="n"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import registerInsert from './register-insert.vue'
+export default {
+  components: { registerInsert },
+  name: 'IndexApp',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  .options-content{
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+  }
+</style>
---
+++ src/components/register-insert.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="content-register">
+      <select id="" v-model="content.operation" class="op-select">
+          <option v-for="op in operations" :key="op" :value="op">{{op}}</option>
+      </select>
+      <input type="text" id="" v-model="content.firstRegister" class="register">
+      <input type="text" id="" v-model="content.secondRegister" class="register">
+      <input v-if="content.operation != 'MOV' && content.operation != 'LDR'" type="text" id="" v-model="content.thirdRegister" class="register">
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            content: {                
+                operation: '',
+                firstRegister: '',
+                secondRegister: '',
+                thirdRegister: '',
+            },
+            operations: [ 
+                "AND", "ADD", "SUB", "MUL", "CMP", "CMN", "EOR", "ORR", "LDR", "MOV"
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+    .content-register{
+        display: flex;
+        justify-content: start;
+        align-content: flex-start;
+        gap: 10px;
+        width: 400px;
+    }
+    .op-select {
+        width: 70px;
+    }
+
+    .register {
+        width: 20%;
+    }
+</style>
